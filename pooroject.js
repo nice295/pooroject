8516935186b2a10da57d14c4a4a7e2c9cfa0fe0d
nice295 <nice295@gmail.com>
2016-01-17 23:25:58 +0900

    updated

--- pooroject.js
+++ pooroject.js
@@ -2,28 +2,41 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.result.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.helpers({
+  Template.result.helpers({
     result: function () {
       var json = Session.get('resultJson');
       var retultHtml = json.building_name;
       return retultHtml;
+    },
+    summary: function () {
+      var json = Session.get('resultJson');
+      return json.desc + " (" + json.cells.length + " 칸)"; 
+    },
+    resultCells: function () {
+      var json = Session.get('resultJson');
+      $("#result-cells").empty();
+      for (i=0 ; i < json.cells.length; i++) {
+        if (json.cells[i].occupied == 1) {
+          $("#result-cells").append('<span class="label label-default">IDLE</span>');
+        }
+        else {
+          $("#result-cells").append('<span class="label label-default">BUSY</span>');         
+        }
+      } 
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    },
+  Template.footer.events({
     'click #fetchButton' : function () {
       console.log("Recent tweets from stream!");
-      $('#fetchButton').attr('disabled','true').val('loading...');
+      //$('#fetchButton').attr('disabled','true').text('Loading...');
+      $('#fetchButton').attr('disabled','true').html('<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Loading...');
       id = $('#id').val();
       Meteor.call('fetchFromService', id, function(err, respJson) {
         if(err) {
@@ -31,9 +44,11 @@ if (Meteor.isClient) {
           console.log("error occured on receiving data on server. ", err );
         } else {
           console.log("respJson: ", respJson[0]);
+          console.log("occupied: ", respJson[0].cells[0].occupied);
           Session.set('resultJson', respJson[0]);
         }
-        $('#fetchButton').removeAttr('disabled').val('Fetch');
+        //$('#fetchButton').removeAttr('disabled').text('REFRESH');
+        $('#fetchButton').removeAttr('disabled').html('<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Refresh');
       });
     }
   });
