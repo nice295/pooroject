5ab778f95e022d83af1857600f51f8e2dad09e7c
nice295 <nice295@gmail.com>
2016-01-19 00:14:15 +0900

    init

--- pooroject.js
+++ pooroject.js
@@ -1,6 +1,18 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault("building", "R3");
+  Session.setDefault("floor", "27");
+  Session.setDefault("url", "http://52.192.77.6:3000/floors/24  .json"); // R3 27th
+
+  Meteor.call('fetchFromService', Session.get('url'), function(err, respJson) {
+    if(err) {
+      window.alert("Error: " + err.reason);
+      console.log("error occured on receiving data on server. ", err );
+    } else {
+      console.log("respJson: ", respJson[0]);
+      Session.set('resultJson', respJson[0]);
+    }
+    $('#fetchButton').removeAttr('disabled').html('<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Refresh');
+  });      
 
   Template.result.helpers({
     counter: function () {
@@ -23,7 +35,7 @@ if (Meteor.isClient) {
       $("#result-cells").empty();
       for (i=0 ; i < json.cells.length; i++) {
         if (json.cells[i].occupied == 1) {
-          $("#result-cells").append('<span class="label label-default">IDLE</span>');
+          $("#result-cells").append('<span class="label label-success">IDLE</span>');
         }
         else {
           $("#result-cells").append('<span class="label label-default">BUSY</span>');         
@@ -32,19 +44,83 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.footer.events({
-    'click #fetchButton' : function () {
-      console.log("Recent tweets from stream!");
-      //$('#fetchButton').attr('disabled','true').text('Loading...');
-      $('#fetchButton').attr('disabled','true').html('<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Loading...');
-      id = $('#id').val();
-      Meteor.call('fetchFromService', id, function(err, respJson) {
+  Template.build_tap.events({
+    'click #select-building' : function () {
+      var clickElement = event.target;
+      console.log("select-building", clickElement.value);
+      //Session.set("building", clickElement.value)
+    },
+    'change #select-floor' : function () {
+      var changeElement = event.target;
+      console.log("select-floor", changeElement.value);
+      Session.set("floor", changeElement.value);
+
+      if (Session.get("building") == "R3") {
+        if (Session.get("floor") == "27") {
+          Session.set("url", "http://52.192.77.6:3000/floors/24.json");
+        }
+        else if (Session.get("floor") == "26") {
+          Session.set("url", "http://52.192.77.6:3000/floors/25.json");
+        }
+        else if (Session.get("floor") == "25") {
+          Session.set("url", "http://52.192.77.6:3000/floors/23.json");
+        }
+        else if (Session.get("floor") == "24") {
+          Session.set("url", "http://52.192.77.6:3000/floors/22.json");
+        }
+        else if (Session.get("floor") == "21") {
+          Session.set("url", "http://52.192.77.6:3000/floors/21.json");
+        }
+        else if (Session.get("floor") == "20") {
+          Session.set("url", "http://52.192.77.6:3000/floors/20.json");
+        }  
+        else if (Session.get("floor") == "19") {
+          Session.set("url", "http://52.192.77.6:3000/floors/19.json");
+        }
+        else if (Session.get("floor") == "18") {
+          Session.set("url", "http://52.192.77.6:3000/floors/18.json");
+        }
+        else if (Session.get("floor") == "17")
+          Session.set("url", "http://52.192.77.6:3000/floors/17.json");
+        else if (Session.get("floor") == "16")
+          Session.set("url", "http://52.192.77.6:3000/floors/16.json");
+        else if (Session.get("floor") == "15")
+          Session.set("url", "http://52.192.77.6:3000/floors/15.json");
+        else if (Session.get("floor") == "14")
+          Session.set("url", "http://52.192.77.6:3000/floors/14.json");
+        else if (Session.get("floor") == "12")
+          Session.set("url", "http://52.192.77.6:3000/floors/13.json");
+        else if (Session.get("floor") == "11")
+          Session.set("url", "http://52.192.77.6:3000/floors/12.json");
+      }
+      else {
+
+      }
+      console.log("url", Session.get('url'));
+
+      Meteor.call('fetchFromService', Session.get('url'), function(err, respJson) {
         if(err) {
           window.alert("Error: " + err.reason);
           console.log("error occured on receiving data on server. ", err );
         } else {
           console.log("respJson: ", respJson[0]);
-          console.log("occupied: ", respJson[0].cells[0].occupied);
+          Session.set('resultJson', respJson[0]);
+        }
+        $('#fetchButton').removeAttr('disabled').html('<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Refresh');
+      });
+    }
+  });
+
+  Template.footer.events({
+    'click #fetchButton' : function () {
+      $('#fetchButton').attr('disabled','true').html('<span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Loading...');
+
+      Meteor.call('fetchFromService', Session.get('url'), function(err, respJson) {
+        if(err) {
+          window.alert("Error: " + err.reason);
+          console.log("error occured on receiving data on server. ", err );
+        } else {
+          console.log("respJson: ", respJson[0]);
           Session.set('resultJson', respJson[0]);
         }
         //$('#fetchButton').removeAttr('disabled').text('REFRESH');
@@ -60,8 +136,9 @@ if (Meteor.isServer) {
   });
 
   Meteor.methods({
-    fetchFromService: function(id) {
-      var url = "http://52.192.77.6:3000/floors/24.json";
+    fetchFromService: function(url) {
+      console.log('url', url);
+
       //synchronous GET
       var result = Meteor.http.get(url, {timeout:30000});
       if(result.statusCode==200) {
